40b5f4b97effafa59a97ec176e53e58b4aa73007
abdtriedcoding <siddabdullahofficial@gmail.com>
2024-10-14 19:09:00 +0530

    add search functionality

--- components/navigation/channel-sidebar.tsx
+++ components/navigation/channel-sidebar.tsx
@@ -1,7 +1,23 @@
 import { auth } from "@/auth";
 import prisma from "@/lib/prisma";
 import { redirect } from "next/navigation";
+import { ChannelType } from "@prisma/client";
+import { Search } from "@/components/search";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { ChannelHeader } from "@/components/navigation/channel-header";
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
+
+const channelIcons = {
+  [ChannelType.TEXT]: <Hash className="h-4 w-4 mr-2" />,
+  [ChannelType.AUDIO]: <Mic className="h-4 w-4 mr-2" />,
+  [ChannelType.VIDEO]: <Video className="h-4 w-4 mr-2" />,
+};
+
+const roleIcons = {
+  GUEST: null,
+  MODERATOR: <ShieldCheck className="w-4 h-4 mr-2 text-indigo-500" />,
+  ADMIN: <ShieldAlert className="w-4 h-4 mr-2 text-rose-500" />,
+};
 
 export async function ChannelSidebar({ serverId }: { serverId: string }) {
   const session = await auth();
@@ -33,28 +49,67 @@ export async function ChannelSidebar({ serverId }: { serverId: string }) {
     return redirect("/");
   }
 
-  // TODO: need to render this on channel sidebar
-  // const textChannels = server?.channels.filter(
-  //   (channel) => channel.type === "TEXT"
-  // );
+  const textChannels = server?.channels.filter(
+    (channel) => channel.type === "TEXT"
+  );
 
-  // const audioChannels = server?.channels.filter(
-  //   (channel) => channel.type === "AUDIO"
-  // );
+  const audioChannels = server?.channels.filter(
+    (channel) => channel.type === "AUDIO"
+  );
 
-  // const videoChannels = server?.channels.filter(
-  //   (channel) => channel.type === "VIDEO"
-  // );
+  const videoChannels = server?.channels.filter(
+    (channel) => channel.type === "VIDEO"
+  );
 
-  // const members = server?.members?.filter(
-  //   (member) => member.userId !== user.id
-  // );
+  const members = server?.members?.filter(
+    (member) => member.userId !== user.id
+  );
 
   const role = server.members.find((member) => member.userId === user.id)?.role;
+
+  const searchData = [
+    {
+      label: "Text Channels",
+      data: textChannels.map((channel) => ({
+        id: channel.id,
+        name: channel.name,
+        icon: channelIcons[channel.type],
+      })),
+    },
+    {
+      label: "Voice Channels",
+      data: audioChannels.map((channel) => ({
+        id: channel.id,
+        name: channel.name,
+        icon: channelIcons[channel.type],
+      })),
+    },
+    {
+      label: "Video Channels",
+      data: videoChannels.map((channel) => ({
+        id: channel.id,
+        name: channel.name,
+        icon: channelIcons[channel.type],
+      })),
+    },
+    {
+      label: "Members",
+      data: members?.map((member) => ({
+        id: member.id,
+        name: member.user.name,
+        icon: roleIcons[member.role],
+      })),
+    },
+  ];
 
   return (
     <div className="min-h-screen flex flex-col text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
       <ChannelHeader server={server} role={role || ""} />
+      <ScrollArea className="flex-1 px-3">
+        <div className="pt-2">
+          <Search searchData={searchData} />
+        </div>
+      </ScrollArea>
     </div>
   );
 }
---
+++ components/search.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import * as React from "react";
+import { SearchIcon } from "lucide-react";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+
+interface SearchProps {
+  searchData: {
+    label: string;
+    data:
+      | {
+          id: string;
+          name: string | null;
+          icon: JSX.Element | null;
+        }[];
+  }[];
+}
+
+export function Search({ searchData }: SearchProps) {
+  const [open, setOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  return (
+    <>
+      <button
+        onClick={() => setOpen(true)}
+        className="group px-2 py-2 rounded-md flex gap-x-2 items-center w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+      >
+        <SearchIcon className="h-4 w-4 text-zinc-500 dark:text-zinc-400" />
+        <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
+          Search
+        </p>
+        <kbd
+          className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] 
+            font-medium text-muted-foreground"
+        >
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search all channels and members" />{" "}
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          {searchData.map(({ label, data }) => {
+            if (!data?.length) return null;
+            return (
+              <CommandGroup key={label} heading={label}>
+                {data.map(({ id, name, icon }) => (
+                  // TODO: add routes
+                  <CommandItem key={id} onSelect={() => setOpen(false)}>
+                    {icon}
+                    <span className="text-sm font-semibold">{name}</span>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            );
+          })}
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+}
